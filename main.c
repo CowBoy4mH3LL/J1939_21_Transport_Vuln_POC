ad176d26b014e490649ee9da6b190bb8980b47e3
j4l-Shvn <subhojeet.m@gmail.com>
2022-03-02 10:41:51 +0530

    Temp commit...

--- main.c
+++ main.c
@@ -1,34 +1,34 @@
-/* Handles the system state machine and makes related calls to modular challenge problem functions */
+// /* Handles the system state machine and makes related calls to modular challenge problem functions */
 
-#include "can_j1939.h"
-#include "transport.h"
-#include "common.h"
-#include "conf.h"
+// #include "can_j1939.h"
+// #include "transport.h"
+// #include "common.h"
+// #include "conf.h"
 
-void terminate_gracefully(int sig, siginfo_t *info, void *vp){
-    if (sig == SIGINT || sig == SIGABRT || sig == SIGTERM){
-        printf("Done. Entering terminal state!!\n");
-        state = STATE_TERMINAL;
-    }
-}
+// void terminate_gracefully(int sig, siginfo_t *info, void *vp){
+//     if (sig == SIGINT || sig == SIGABRT || sig == SIGTERM){
+//         printf("Done. Entering terminal state!!\n");
+//         state = STATE_TERMINAL;
+//     }
+// }
 
-void setup(char *iface_type, char *iface_num){
+// void setup(char *iface_type, char *iface_num){
 
-    initiate_signals(terminate_gracefully);
+//     initiate_signals(terminate_gracefully);
 
-    //Setup CAN
-    can_setup(iface_type, iface_num);
+//     //Setup CAN
+//     can_setup(iface_type, iface_num);
 
-    //Setup everything else
-    transport_setup();
-}
+//     //Setup everything else
+//     transport_setup();
+// }
 
-void loop() {
-    if (can_read(0) > 0){
-        parse_J1939();
-        transport_handler();
-    }
-}
+// void loop() {
+//     if (can_read(0) > 0){
+//         parse_J1939();
+//         transport_handler();
+//     }
+// }
 
 int main(int argc, char * argv[]){
     // Check parameters
@@ -39,12 +39,12 @@ int main(int argc, char * argv[]){
 
     setup(argv[1], argv[2]);
 
-    while (state != STATE_TERMINAL){
-        loop();
-    }
+//     while (state != STATE_TERMINAL){
+//         loop();
+//     }
 
-    can_takedown();
-    transport_takedown();
+//     can_takedown();
+//     transport_takedown();
 
     printf("Exiting!!");
     return 0;
--- transport.h
+++ transport.h
@@ -1,248 +1,56 @@
-//
-// Created by subhojeet on 7/6/21.
-//
+// /*
+// For sender:: 
+//     free --[sent rts]--> initiated --[recieved CTS]--> established + (send data) --[sent packets/bytes or recv eoma]--> free;
+// For reciever::
+//     free --[recv rts]--> (allocate/reallocate, send cts) + established + (recv data) --[recf packets/bytes]--> free;
+// */
+// enum state{FREE, INITIATED, ESTABLISHED};
+// enum action{SENT_RTS, RECV_RTS, RECV_CTS, TERM}
+// // /*
+// // free -
+// // */
+// // enum conn_state{FREE, SENT_RTS, SENT_CTS, RECV_CTS};
 
-#ifndef TRANSPORTLAYERISSUES_TRANSPORT_H
-#define TRANSPORTLAYERISSUES_TRANSPORT_H
+// #define TPCM 0x00ec0000
+// #define TPCM_MSG 0x18ECFF00
+// #define TPDT 0x00eb0000
+// #define RQST 0x00ea0000
+// #define RTS 0x10
+// #define CTS 0x11
+// #define SRC_MASK 0x000000ff
+// #define DST_MASK 0x0000ff00
+// #define PF_MASK  0x00ff0000
 
-#include <byteswap.h>
-#include <math.h>
-#include "can_j1939.h"
-#include "conf.h"
-enum state{RCVD_RQST, SENT_RTS, SENT_CTS, CONN_ESTABLISHED} ;
-#define TPCM 0x00ec0000
-#define TPCM_MSG 0x18ECFF00
-#define TPDT 0x00eb0000
-#define RQST 0x00ea0000
-#define RTS 0x10
-#define CTS 0x11
-#define SRC_MASK 0x000000ff
-#define DST_MASK 0x0000ff00
-#define PF_MASK  0x00ff0000
+// void create_or_update_session(action act){
 
-/* Globals */
-struct ConnectionInfo{
-    int type; //0 -> sending, 1 -> receiving // Not used actively now
-    enum state state; //1 -> Sent RTS, 2 -> Sent CTS, 3 -> Connection established
-    uint32_t pgn;
-    uint16_t size;
-    uint8_t num_packets;
-    uint8_t recv_num_packets;
-    uint8_t* data;
-    uint16_t data_pos;
-    uint8_t sender_src;
-};
-struct ConnectionInfo* connection_infos[NUM_DEVS];
-uint8_t src = SRC;
-struct can_frame CTS;
-struct can_frame RTS;
-struct can_frame DUMMY_DATA;
-uint16_t size;
-uint8_t num_packets;
-uint32_t pgn;
+//     if (connection_infos[src] == NULL){
+//         connection_infos[src] = malloc(sizeof(struct ConnectionInfo));
+//         connection_infos[src]->state = RCVD_RQST;
+//     }
 
+//     if (connection_infos[src]->state == CONN_ESTABLISHED) /* Vuln 3. */
+//         return;
 
-/* Support functions */
+//     size = (int)ceil((float)size/7)*7; //scale size so we can fit the FF's and buffer overflows cannot be caused by sending less than 7 bytes of data in a single frame
+//     if (connection_infos[src]->state == SENT_CTS){
+//         /* Vuln 2. */
+//         connection_infos[src]->data = realloc(connection_infos[src]->data, size);
+//     }
+//     else{
+//         // if (connection_infos[src]->data != NULL) // Patch for Vuln 1.
+//         connection_infos[src]->data = malloc(size); /* Vuln 1. */
+//     }
 
-void print_session(src){
-    printf ("------------ New Connection with %x at 0x%x -------------\n", src, connection_infos[src]);
-    printf("connection_infos[%d]->type = %d\n", src, connection_infos[src]->type);
-    printf("connection_infos[%d]->state = %d\n", src, connection_infos[src]->state);
-    printf("connection_infos[%d]->pgn = %x\n", src, connection_infos[src]->pgn);
-    printf("connection_infos[%d]->size = %d\n", src, connection_infos[src]->size);
-    printf("connection_infos[%d]->num_packets = %d\n", src, connection_infos[src]->num_packets);
-    printf("connection_infos[%d]->recv_num_packets = %d\n", src, connection_infos[src]->recv_num_packets);
-    printf("connection_infos[%d]->sender_src = %x\n", src, connection_infos[src]->sender_src);
-    printf("connection_infos[%d]->data at 0x%x is %x\n", src, connection_infos[src]->data, *connection_infos[src]->data);
-    printf("connection_infos[%d]->data_pos = %d\n", src, connection_infos[src]->data_pos);
-    printf ("--------------------------------------------------------------\n");
-}
+//     memset(connection_infos[src]->data , 0, size);
 
-void delete_connection(src){
-      if (connection_infos[src]->data != NULL){
-            free(connection_infos[src]->data);
-            connection_infos[src]->data = NULL;
-        }
-    free(connection_infos[src]);
-    connection_infos[src] = NULL;
-}
+//     connection_infos[src]->type = type;
+//     connection_infos[src]->data_pos = 0;
+//     connection_infos[src]->state = state;
+//     connection_infos[src]->pgn = pgn;
+//     connection_infos[src]->size = size;
+//     connection_infos[src]->num_packets = num_packets;
+//     connection_infos[src]->recv_num_packets = 0;
+//     connection_infos[src]->sender_src = src;
+//     print_session(src);
 
-uint8_t** RTS_alarm_queue[((int)1250/BITWIDTH*88) + 1]; //A request is worth 88 bits
-long RTS_alarm_queue_index = -1;
-void alarm_handle(){
-    // J1939 specifies scheduled timers here but the max limit there is 1250 ms. 
-    // This means that an attacker will full CAN access i.e. able to send appx 4 frames per ma will send appx. 1250*4 = 5000 requests
-    // If the data is close to 44 bytes this will result in reserving 44*1250*4 = 220 KB of heap data.
-    // For systems with 256 KB of memory, and more than half reserved for program, stack frames etc. this may simply crash the system anyways
-    // This alarm function works but is not a defense really.
-
-    free(RTS_alarm_queue[RTS_alarm_queue_index]);
-    RTS_alarm_queue[RTS_alarm_queue_index] = NULL;
-    RTS_alarm_queue_index++;
-}
-void schedule_RTS_alarm(uint8_t** data_pointer){
-    RTS_alarm_queue_index++;
-    if (RTS_alarm_queue_index >= ((int)1250/BITWIDTH*88) + 1)
-        RTS_alarm_queue_index = 0;
-    RTS_alarm_queue[RTS_alarm_queue_index] = data_pointer;
-}
-
-void create_or_update_session(int state, int type){
-    if (connection_infos[src] == NULL){
-        connection_infos[src] = malloc(sizeof(struct ConnectionInfo));
-        connection_infos[src]->state = RCVD_RQST;
-    }
-
-    if (connection_infos[src]->state == CONN_ESTABLISHED) /* Vuln 3. */
-        return;
-
-    size = (int)ceil((float)size/7)*7; //scale size so we can fit the FF's and buffer overflows cannot be caused by sending less than 7 bytes of data in a single frame
-    if (connection_infos[src]->state == SENT_CTS){
-        /* Vuln 2. */
-        connection_infos[src]->data = realloc(connection_infos[src]->data, size);
-    }
-    else{
-        // if (connection_infos[src]->data != NULL) // Patch for Vuln 1.
-        connection_infos[src]->data = malloc(size); /* Vuln 1. */
-    }
-
-    memset(connection_infos[src]->data , 0, size);
-
-    connection_infos[src]->type = type;
-    connection_infos[src]->data_pos = 0;
-    connection_infos[src]->state = state;
-    connection_infos[src]->pgn = pgn;
-    connection_infos[src]->size = size;
-    connection_infos[src]->num_packets = num_packets;
-    connection_infos[src]->recv_num_packets = 0;
-    connection_infos[src]->sender_src = src;
-    print_session(src);
-
-}
-
-uint16_t check_pgn_size(){ //Returns the size if the PGN is available, else 0
-    return 44; //TODO change the default later
-}
-
-/* Main functions */
-void transport_setup(){
-
-    memset(connection_infos, 0, NUM_DEVS*sizeof(struct ConnectionInfo));
-    //Init default RTS
-    RTS.can_dlc = 8;
-    RTS.can_id = TPCM_MSG | SRC;
-    memset(RTS.data, 0xff, 8);
-    RTS.data[0] = 0x10;
-
-    //Init default CTS
-    CTS.can_dlc = 8;
-    CTS.can_id = TPCM_MSG | SRC;
-    memset(CTS.data, 0xff, 8);
-    CTS.data[0] = 0x11;
-    CTS.data[2] = 1;
-}
-
-void transport_handler(){
-    //Get the sender
-    src = read_frame.can_id & SRC_MASK;
-
-    if (((read_frame.can_id & DST_MASK) >> 8) == SRC) { //TODO add broadcast support later
-        switch (read_frame.can_id & PF_MASK){
-            case TPCM: //TP.CM
-                //Get the PGN
-                memcpy (&pgn, &read_frame.data[5], 3); 
-                pgn = bswap_32(pgn) >> 8;
-
-                switch (read_frame.data[0]) {
-                    case RTS: //RTS recv, CTS send
-                        //Get size and num_packets
-                        memcpy(&size, &read_frame.data[1], 2);
-                        size = bswap_16(size);
-                        num_packets = read_frame.data[3]; /* Vuln 2. */
-                        /* Patch of vuln 2.0 */
-                        // num_packets = (int)ceil((float)size/7);
-
-                        // Create a session
-                        create_or_update_session(2, 1);
-
-                        if (connection_infos[src]->state == CONN_ESTABLISHED){
-                            printf("Duplicate connection request from SA = 0x%x. Not responding!!", src);
-                            break;
-                        }
-
-                        CTS.can_id = (CTS.can_id & 0xffff00ff) | (src << 8);
-                        CTS.data[1] = read_frame.data[4];
-                        memcpy (&CTS.data[5], &read_frame.data[5], 3); 
-                        can_write(&CTS, 1);
-                        break;
-                    case CTS: //CTS recieve, connection established
-                        if (connection_infos[src]->state == SENT_RTS)
-                            connection_infos[src]->state = CONN_ESTABLISHED;
-                        
-                        connection_infos[src]->data_pos = (read_frame.data[1] - 1)*7;
-                        break;
-                }
-                break;
-            case TPDT: //Data recieve
-                if (connection_infos[src] == NULL)
-                    break;
-
-                if (connection_infos[src]->state == SENT_CTS)
-                        connection_infos[src]->state = CONN_ESTABLISHED;
-                
-                if (connection_infos[src]->state == CONN_ESTABLISHED){
-                    connection_infos[src]->recv_num_packets++;
-                    printf ("Recieved packet %d for connection with %d\n", connection_infos[src]->recv_num_packets, src);
-                   
-                    memcpy(&connection_infos[src]->data[(read_frame.data[0] -1)*7], &read_frame.data[1], 7);
-
-                    if (connection_infos[src]->recv_num_packets >= connection_infos[src]->num_packets){
-                        //Presumably pass the data to the application layer providing the size along with the data, something like this callback(connection_infos[src]->data, size) so that layer
-                         printf ("Recieved all packets for connection with %d. Closing connection!!\n", connection_infos[src]->recv_num_packets, src);
-                        delete_connection(src);
-                    }
-                    
-                    /* Possible patch of Vuln 2.*/
-                    // connection_infos[src]->data_pos = connection_infos[src]->data_pos + 7;
-                    // if (connection_infos[src]->data_pos >= connection_infos[src]->size){
-                    //     free(connection_infos[src]->data);
-                    //     free(connection_infos[src]);
-                    // }
-                }
-                break;
-
-            case RQST: //Request recieve; RTS sent
-                //Get the PGN
-                memcpy (&pgn, &read_frame.data[0], 3); 
-                pgn = bswap_32(pgn) >> 8;
-
-                //Check if PGN is available
-                if ((size = check_pgn_size()) != -1){
-                    // Create a session
-                    create_or_update_session(1, 0); /* Vuln 1. */
-                    schedule_RTS_alarm(connection_infos[src]->data);
-
-                    //Send RTS
-                    RTS.can_id = (RTS.can_id & 0xffff00ff) | (src << 8);
-                    RTS.data[3] = (int)ceil((float)size/7);
-                    size = bswap_16(size);
-                    memcpy(&RTS.data[1], &size, 2);
-                    memcpy(&RTS.data[5], &read_frame.data[0], 3);
-                    can_write(&RTS, 1);
-                }
-                //TODO add else to send negative ack on unavailibility of PGN
-                break;
-        }
-    }
-    
-}
-
-void transport_takedown(){
-    for (int i = 0; i < NUM_DEVS; i++){
-        if (connection_infos[i] != NULL){
-            delete_connection(i);
-        }
-    }
-}
-
-#endif //TRANSPORTLAYERISSUES_TRANSPORT_H
+// }
